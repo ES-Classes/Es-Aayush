b1f536ff79a9e2b07856070711ee0218e5154655
Aayush_Timalsina <63510584+Aayush939@users.noreply.github.com>
2022-04-03 11:31:30 +0545

    chnage code

--- class/lession1.js
+++ class/lession1.js
@@ -1,2 +1,13 @@
 //INstall nodejs , github org  configuration
 //Install git for windows 
+console.log("Hello there!")
+
+var age= 20;
+age ='Aayush';
+//datatype -8
+//numbers-FP (floating point)
+ let population = 156546846;
+ console.log(1/0);
+ console.log("Asayush"/0);
+ var bigNumber = 16546346464516731644611543536165146463446346486416546745416339789456684654549645648978454878487n;
+ console.log(bigNumber);
