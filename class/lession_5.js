04587068ebea75d8c8999d045788ab4f0de2e5c7
Aayush_Timalsina <63510584+Aayush939@users.noreply.github.com>
2022-04-10 10:52:58 +0545

    new things

--- class/lession_5.js
+++ class/lession_5.js
@@ -11,4 +11,23 @@ let isEven = x%2==0; // 13 % 2 = 1
 isEven = x & 1 == 0;
 
 let p = x << 1;
+let q =y>> 1;
+let r = ~isEven;
 
+//  Comprision
+let s = x == 90;
+
+ let k= x === "90";
+  console.log(k) ;
+
+  let ss = x !=='90';
+  s=x<= '90'
+//logical 
+let t = s && ss;
+t= false &&  anything;
+t= s || ss;
+t=truev|| ss;
+
+const u = x ?? "Aayush" // nullish ( null or undefine) coalescing operator
+
+Let  = x === 90 ? 45:89;
