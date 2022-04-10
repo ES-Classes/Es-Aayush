189089ef18513dbf9b2114aa07a33f0ba8bf3986
Aayush_Timalsina <63510584+Aayush939@users.noreply.github.com>
2022-04-10 10:49:53 +0545

    new thing

---
+++ class/lession_5.js
@@ -0,0 +1,14 @@
+let y=90;
+let x= 90;
+let x1= x++;
+let y1=++y;
+console.log(x,y,x1,y1);
+//unary operator: ++,--,!
+let z=!x;
+// find if number is even of odd
+
+let isEven = x%2==0; // 13 % 2 = 1
+isEven = x & 1 == 0;
+
+let p = x << 1;
+
