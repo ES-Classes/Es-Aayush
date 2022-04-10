3a8fc60745860b1dbb6f88c9fc53a8fcae87b235
Aayush_Timalsina <63510584+Aayush939@users.noreply.github.com>
2022-04-10 14:33:09 +0545

    new lession

---
+++ class/lession-5_part-2.js
@@ -0,0 +1,18 @@
+ const a= "vedas";
+ const b= 'Vedas'
+ const c= `vedas`
+ const d = new String('vedas');
+ console.log(a.length);
+ console.log(a.charAt(1));
+ console.log(a[1]);
+
+ let cc='क';
+ let ccc='क';
+//  console.log(cc.localComapre(ccc));
+ let para ="BHBKHvkjgvkgjvckgjvchjvghvgk\
+ jKeyboardEventkjbjkbjkb";
+ para= `lhbkhvjkvj
+ jhvgkvv`;
+// string interpolation
+console.log(`my college is ${a}`);
+
